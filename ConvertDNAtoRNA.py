46134eae6823f394c6f8c007331cd2a7658d5d0a
Colin <58573232+speercolin@users.noreply.github.com>
2023-04-27 23:24:06 -0400

    Delete ConvertDNAtoRNA.py

--- ConvertDNAtoRNA.py
+++
@@ -1,8 +0,0 @@
-import string
-
-# Method that converts Thymine into Uracil.
-def convert_to_mRNA(string):
-    converted_dna = string.replace('T', 'U')
-    print("DNA sequence successfully converted into mRNA. ")
-    print(converted_dna)
-
